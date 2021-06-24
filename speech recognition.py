6ffbdbc6386eada8548aa96e9630632e59fddc90
DeadBat <33771365+CaptDeadBat@users.noreply.github.com>
2021-06-24 19:27:09 +0530

    Add files via upload

---
+++ speech recognition.py
@@ -0,0 +1,44 @@
+# import required module
+
+import speech_recognition as sr
+
+
+
+
+# explicit function to take input commands
+# and recognize them
+def takeCommandHindi(lang=0):
+        if(lang==1):
+                l='en-In'
+        elif(lang==0):
+                l='hi-In'
+                
+        r = sr.Recognizer()
+        
+        with sr.Microphone() as source:
+                
+                
+                # seconds of non-speaking audio before
+                # a phrase is considered complete
+                print('Listening')
+                
+                r.pause_threshold = 0.5
+                
+                audio = r.listen(source)
+                
+                try:
+                        print("Recognizing")
+                        Query = r.recognize_google(audio, language=l)
+                        
+                        # for listening the command in indian english
+                        print("Command Recognized ='", Query, "'")
+                
+                # handling the exception, so that assistant can
+                # ask for telling again the command
+                except Exception as e:
+                        print(e)
+                        print("Say that again sir")
+                        return "None"
+                return Query
+
+takeCommandHindi()
